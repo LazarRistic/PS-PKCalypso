67d88d5d3cde2559d1fdd122a9bc6f994cd349d2
LazarRistic <rista_junior@eunet.rs>
2015-02-14 22:21:57 +0100

    Create Classes.php

---
+++ basic_information/Classes.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lazar
+ * Date: 2/14/15
+ * Time: 9:49 PM
+ */
+
+namespace root\basic_information;
+
+
+class Classes {
+
+    /**
+     * Id of dancer's class
+     * @var int
+     */
+    private $id;
+
+    /**
+     * Class of Dancer
+     * @var String
+     */
+    private $name;
+
+    /**
+     * @param $id
+     * @param $name
+     * @return Classes
+     */
+    public static function addClass($id, $name)
+    {
+        $class = new Classes();
+        $class->id = $id;
+        $class->name = $name;
+        return $class;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param String $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return String
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+
+} 
